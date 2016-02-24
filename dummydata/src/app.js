da53751daaeaa71382f6b981727e948eb24aabf0
Jay Bienvenu <github@bienv.com>
2016-02-24 10:23:41 -0600

    Can't gitignore the JSPM files because they have to go to Github.

--- dummydata/src/app.js
+++ dummydata/src/app.js
@@ -1,3 +1,11 @@
 export class App {
   message = 'Welcome to Aurelia!';
+
+  language = 'English';
+  country = 'UnitedStates';
+  ordinal = 0;
+
+  submit() {
+    //TODO Generate new data.
+  }
 }
