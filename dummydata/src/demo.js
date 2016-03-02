1df5376ff1d0bbb9c9db74f0b298dbc69229614f
Jay Bienvenu <github@bienv.com>
2016-03-02 11:14:15 -0600

    Move data definitions to demo.js. Clean out context.js.

--- dummydata/src/demo.js
+++ dummydata/src/demo.js
@@ -1,3 +1,46 @@
+import {inject} from 'aurelia-framework';
+import {Context} from './core/context';
+import {DummyData} from './core/data';
+
+import English from './languages/english';
+// import German from './languages/german';
+// import Greek from './languages/greek';
+import Japanese from './languages/japanese';
+// import Spanish from './languages/spanish';
+
+import UnitedStates from './countries/united-states';
+// import Canada from './countries/canada';
+
+@inject(Context)
 export class Demo {
+
+  context = {};
   menu = [["Title",function() { return "result"; }]];
+
+  populate() {
+    this.menu = this._data.resetMenu();
+    // menu should be an array, all elements of which should be [title,function] or null.
+  }
+
+  constructor(context) {
+    console.log("Demo.constructor");
+    this.context = new Context();
+
+    this._data = new DummyData(this);
+
+    this._data.addLanguage({
+      'English': English,
+      // 'German': new German(),
+      // 'Greek': new Greek(),
+      'Japanese': Japanese,
+      // 'Spanish': new Spanish()
+    });
+
+    this._data.addCountry({
+      'UnitedStates': UnitedStates
+      // 'Canada': new Canada(),
+    });
+
+    this.populate();
+  }
 }
