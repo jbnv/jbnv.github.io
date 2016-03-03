0b84a7553b4daa98ecebebe20de42e7f2a40c9d8
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-03 10:10:27 -0600

    Update the German generator.

--- dummydata/src/languages/german.js
+++ dummydata/src/languages/german.js
@@ -1,29 +1,38 @@
-var German = function(dd) {
+import { DummyDataEngine } from "../core/engine"
+import djoin from "../util/djoin"
+import "../util/extensions"
 
-  var _data = new DummyDataEngine(dd,'Languages','German','json');
+var _data = function() {};
+let _enginePromise = new DummyDataEngine('Languages','German','json');
+_enginePromise.then(data => _data = data).done();
 
-  var maleNameSelector = function() { return _data('GermanMaleNames'); };
+function _maleName() { return _data('GermanMaleNames',{capitalize:true}); }
 
-  function maleName() { return toInitialCase(maleNameSelector()); }
+function _femaleName() { return _data('GermanFemaleNames',{capitalize:true}); }
 
-  var femaleNameSelector = function() { return _data('GermanFemaleNames'); };
+function _surname() {
+  let prefix = _data('GermanSurnamePrefixes',{capitalize:true});
+  let suffix = _data('GermanSurnameSuffixes');
+  if (prefix && suffix) return (prefix+suffix);
+  return null;
+}
 
-  function femaleName() { return toInitialCase(femaleNameSelector()); }
+function _maleFullName() { return djoin(_maleName," ",_surname); };
 
-  var surnameSelector = function() { return _data('GermanSurnamePrefixes')+_data('GermanSurnameSuffixes'); }
+function _femaleFullName() { return djoin(_femaleName," ",_surname); };
 
-  function surname() { return toInitialCase(surnameSelector()); }
+export default {
+  maleName: _maleName,
+  femaleName: _femaleName,
+  surname: _surname,
+  maleFullName: _maleFullName,
+  femaleFullName: _femaleFullName,
 
-  function maleFullName() { return maleName()+" "+surname(); };
-
-  function femaleFullName() { return femaleName()+" "+surname(); };
-
-  this.menuItems = [
-    ["Male Name", maleName],
-    ["Female Name", femaleName],
-    ["Surname", surname],
-    ["Male Full Name", maleFullName],
-    ["Female Full Name", femaleFullName]
-  ];
-
-};
+  menuItems: [
+    ["Male Name", _maleName],
+    ["Female Name", _femaleName],
+    ["Surname", _surname],
+    ["Male Full Name", _maleFullName],
+    ["Female Full Name", _femaleFullName]
+  ]
+}
