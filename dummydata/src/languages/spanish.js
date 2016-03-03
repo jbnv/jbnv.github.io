ec1bc8a9b3d1a208620aa591004e215bca87bcb9
Jay Bienvenu <bienvenunet@yahoo.com>
2016-03-03 10:22:15 -0600

    Fix Spanish.

--- dummydata/src/languages/spanish.js
+++ dummydata/src/languages/spanish.js
@@ -1,77 +1,88 @@
-var Spanish = function(dd) {
+import { DummyDataEngine } from "../core/engine"
+import { Selector } from "../core/selector"
+import djoin from "../util/djoin"
+import "../util/extensions"
 
-  var _data = new DummyDataEngine(dd,'Languages','Spanish','json');
+var _data = function() {};
+let _enginePromise = new DummyDataEngine('Languages','Spanish','json');
+_enginePromise.then(data => _data = data).done();
 
-  var _this = this;
+var nameSuffix = new Selector([[3,""],[1,"it"],[1,"in"],[1,"ill"]]);
 
-  var nameSuffix = new Selector([[3,""],[1,"it"],[1,"in"],[1,"ill"]]);
-
-  function appendSuffix(stem,suffix) {
-    if (suffix == null || suffix == "") return stem;
-    if (suffix.substr(0,1) == "i" || suffix.substr(0,1) == "e") {
-      switch (stem.substr(-1)) {
-        case "c": stem = stem.substr(0,stem.length-1)+"qu"; break;
-        case "g": stem = stem+"u"; break;
-      }
+function appendSuffix(stem,suffix) {
+  if (suffix == null || suffix == "") return stem;
+  if (suffix.substr(0,1) == "i" || suffix.substr(0,1) == "e") {
+    switch (stem.substr(-1)) {
+      case "c": stem = stem.substr(0,stem.length-1)+"qu"; break;
+      case "g": stem = stem+"u"; break;
     }
-    return stem+suffix;
   }
+  return stem+suffix;
+}
 
-  var placeSelector = new Selector([
-    function() { return _data('SpanishPlaces'); },
-    function() { return "San "+maleName(); },
-    function() { return "Santa "+femaleName(); }
-  ]);
+var placeSelector = new Selector([
+  function() { return _data('SpanishPlaces'); },
+  function() { return "San "+maleName(); },
+  function() { return "Santa "+femaleName(); }
+]);
 
-  var nameStemSelector = new Selector([
-    function() { return _data('SpanishNameStems'); },
-    function() { return _data('SpanishNamePrefixes')+_data('SpanishNameSuffixes'); }
-  ]);
+var nameStemSelector = new Selector([
+  function() { return _data('SpanishNameStems'); },
+  function() { return _data('SpanishNamePrefixes')+_data('SpanishNameSuffixes'); }
+]);
 
-  function properNameStem() {
-    return appendSuffix(nameStemSelector(),nameSuffix());
-  }
+function properNameStem() {
+  return appendSuffix(nameStemSelector(),nameSuffix());
+}
 
-  function ordinalNumberAsName() {
-    return appendSuffix(_data('SpanishOrdinalNumbers'),nameSuffix());
-  }
+function ordinalNumberAsName() {
+  return appendSuffix(_data('SpanishOrdinalNumbers'),nameSuffix());
+}
 
-  var maleNameSelector = new Selector([
-    [1, function() { return _data('SpanishMaleNames'); }],
-    [3, function() { return properNameStem()+"o"; }],
-    [1, function() { return ordinalNumberAsName()+"o"; }]
-  ]);
+var maleNameSelector = new Selector([
+  [1, function() { return _data('SpanishMaleNames'); }],
+  [3, function() { return properNameStem()+"o"; }],
+  [1, function() { return ordinalNumberAsName()+"o"; }]
+]);
 
-  function maleName() { return toInitialCase(maleNameSelector()); }
+function maleName() { return maleNameSelector().toInitialCase(); }
 
-  var femaleNameSelector = new Selector([
-    [3, function() { return _data('SpanishFemaleNames'); }],
-    [3, function() { return properNameStem()+"a"; }],
-    [1, function() { return ordinalNumberAsName()+"a"; }]
-  ]);
+var femaleNameSelector = new Selector([
+  [3, function() { return _data('SpanishFemaleNames'); }],
+  [3, function() { return properNameStem()+"a"; }],
+  [1, function() { return ordinalNumberAsName()+"a"; }]
+]);
 
-  function femaleName() { return toInitialCase(femaleNameSelector()); }
+function femaleName() { return femaleNameSelector().toInitialCase(); }
 
-  var surnameSuffix = new Selector(["o","a","os","as","es","ez"]);
+var surnameSuffix = new Selector(["o","a","os","as","es","ez"]);
 
-  var surnameSelector = new Selector([
-    [3, function() { return _data('SpanishSurnames'); }],
-    [3, function() { return toInitialCase(appendSuffix(properNameStem(),surnameSuffix())); }],
-    [1, function() { return toInitialCase(appendSuffix(ordinalNumberAsName(),surnameSuffix())); }],
-    [2, placeSelector],
-    [2, function() { return "de "+placeSelector(); }]
-  ]);
+var surnameSelector = new Selector([
+  [3, function() { return _data('SpanishSurnames'); }],
+  [3, function() { return appendSuffix(properNameStem(),surnameSuffix()).toInitialCase(); }],
+  [3, function() { return appendSuffix(ordinalNumberAsName(),surnameSuffix()).toInitialCase(); }],
+  [2, placeSelector],
+  [2, function() { return "de "+placeSelector(); }]
+]);
 
-  var surname = new Selector([
-    [9,surnameSelector],
-    function() { return surnameSelector()+" y "+surnameSelector()}
-  ]);
+var surname = new Selector([
+  [9,surnameSelector],
+  function() { return surnameSelector()+" y "+surnameSelector()}
+]);
 
-  function maleFullName() { return maleName()+" "+surname(); };
+function maleFullName() { return maleName()+" "+surname(); };
 
-  function femaleFullName() { return femaleName()+" "+surname(); };
+function femaleFullName() { return femaleName()+" "+surname(); };
 
-  this.menuItems = [
+export default {
+  maleName: maleName,
+  femaleName: femaleName,
+  surname: surname,
+  maleFullName: maleFullName,
+  femaleFullName: femaleFullName,
+  place: placeSelector,
+
+  menuItems: [
     ["Male Name", maleName],
     ["Female Name", femaleName],
     ["Surname", surname],
@@ -79,6 +90,5 @@ var Spanish = function(dd) {
     ["Female Full Name", femaleFullName],
     null,
     ["Place", placeSelector]
-  ];
-
-};
+  ]
+}
